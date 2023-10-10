b3758d304ce7599f58bb4e00b974c568c27537bc
Fumiya Kohzu <kohzu.fumiya.code@gmail.com>
2023-10-10 18:23:11 +0900

    weston git log

---
+++ ubuntu_initial_setteing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y install gnome-tweaks
+sudo apt -y install vim
+sudo apt -y install tmux
+stty stop undef
+git config --global user.email "kohzu.fumiya.code@gmail.com"
+git config --global user.name "Fumiya Kohzu"
+git config --global core.editor vim
+
